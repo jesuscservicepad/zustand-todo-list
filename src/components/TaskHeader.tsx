7662e6a06390606171d25b766c6932d634f5b2ff
Jesus Calderin <jesus.c@servicepad.com>
2023-01-11 12:11:03 -0400

    new icon and title

--- src/components/TaskHeader.tsx
+++ src/components/TaskHeader.tsx
@@ -3,7 +3,7 @@ import TaskForm from "./TaskForm";
 const TaskHeader = () => {
   return (
     <div className="task-header">
-      <h1 className="title">Todo App</h1>
+      <h1 className="title">Todo List</h1>
       <TaskForm />
     </div>
   );
